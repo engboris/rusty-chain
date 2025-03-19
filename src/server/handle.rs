9fca11927a536c08817d512a45c71996fbe42b04
engboris <boris.eng@ocamlpro.com>
2025-03-19 22:30:25 +0100

    Add transaction propagation in server

--- src/server/handle.rs
+++ src/server/handle.rs
@@ -1,14 +1,27 @@
-use anyhow::{Error, Result};
+use anyhow::Result;
 use rusty_chain::core::{
     blockchain::{Block, BlockHeader, Blockchain, NB_TXN_PER_BLOCK},
-    transaction::Transaction,
+    transaction::{Encodable, Transaction},
 };
-use std::{
-    collections::VecDeque,
-    io::{self, prelude::*},
-    net::{TcpListener, TcpStream},
-    time::SystemTime,
-};
+use std::sync::Arc;
+use std::{collections::VecDeque, fs, time::SystemTime};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+
+// Propagates received transactions to other peer nodes in the network
+async fn broadcast(tx: &Transaction) {
+    let peers_addr: Vec<String> = fs::read_to_string("src/server/peers")
+        .unwrap()
+        .lines()
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .collect();
+    for addr in peers_addr {
+        let mut stream = TcpStream::connect(&addr).await.unwrap();
+        stream.write_all(&tx.encode()).await.unwrap();
+    }
+}
 
 fn create_block(blockchain: &mut Blockchain, mempool: &mut VecDeque<Transaction>) {
     log::info!("Creating block...");
@@ -34,23 +47,26 @@ fn create_block(blockchain: &mut Blockchain, mempool: &mut VecDeque<Transaction>
 
 pub async fn decode_from_stream(stream: &mut TcpStream) -> io::Result<Option<Transaction>> {
     let mut buffer = Vec::new();
-    stream.read_to_end(&mut buffer)?;
+    stream.read_to_end(&mut buffer).await.unwrap();
     Ok(Transaction::decode(&buffer))
 }
 
 async fn handle_connection(
     mut stream: TcpStream,
-    mempool: &mut VecDeque<Transaction>,
-    blockchain: &mut Blockchain,
+    mempool: Arc<Mutex<VecDeque<Transaction>>>,
+    blockchain: Arc<Mutex<Blockchain>>,
 ) -> Result<()> {
     log::info!("Handling connection.");
+    let mut mempool = mempool.lock().await;
+    let mut blockchain = blockchain.lock().await;
 
     loop {
         match decode_from_stream(&mut stream).await {
             Err(e) => log::error!("Error: {e}"),
             Ok(None) => return Ok(()),
             Ok(Some(tx)) => {
-                if Blockchain::valid_transaction(blockchain, &tx) {
+                if Blockchain::valid_transaction(&blockchain, &tx) {
+                    broadcast(&tx).await;
                     mempool.push_back(tx.clone());
                     log::info!("Received transaction: {:?}", tx)
                 } else {
@@ -59,24 +75,23 @@ async fn handle_connection(
             }
         }
         if mempool.len() >= NB_TXN_PER_BLOCK {
-            create_block(blockchain, mempool);
-            log::info!("Blockchain has {} blocks.", blockchain.len());
+            create_block(&mut blockchain, &mut mempool);
+            log::info!("Blockchain has {} blocks.", &blockchain.len());
         }
     }
 }
 
 pub async fn create_listener(addr: &str) -> Result<()> {
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind(addr).await.unwrap();
     log::info!("Server listening on {addr}...");
 
-    let mut mempool = VecDeque::<Transaction>::new();
-    let mut blockchain = Blockchain::new();
+    let mempool = Arc::new(Mutex::new(VecDeque::<Transaction>::new()));
+    let blockchain = Arc::new(Mutex::new(Blockchain::new()));
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => handle_connection(stream, &mut mempool, &mut blockchain).await,
-            Err(e) => Err(Error::new(e)),
-        }?;
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let mempool = Arc::clone(&mempool);
+        let blockchain = Arc::clone(&blockchain);
+        tokio::spawn(handle_connection(stream, mempool, blockchain));
     }
-    Ok(())
 }
